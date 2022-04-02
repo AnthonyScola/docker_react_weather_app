0054adca747497c636165c25edd14c825b83f8df
AnthonyScola <anthonyscola@outlook.com>
2022-04-02 10:32:07 -0400

    Fixed error with depricated React 17 elements.

--- src/WeatherCard.js
+++ src/WeatherCard.js
@@ -5,18 +5,18 @@ import cloudy from './res/weather_icons/PNG/cloudy.png';
 export default function WeatherCard() {
   const [weatherConds, setWeather] = useState([day_clear,77,45,5]);
   return (
-    <div class='col'>
-        <div class="card">
-            <div class="card-body">
-                <h2 class="card-title">Today</h2>
+    <div className='col'>
+        <div className="card">
+            <div className="card-body">
+                <h2 className="card-title">Today</h2>
                 <img src={weatherConds[0]} alt="Weather Icon" width="100%"/>
                 <p>
                   High: {weatherConds[1]}° F <br/>
                   Low: {weatherConds[2]}° F
                 </p>
                 <hr/>
-                <p class="card-text">Some quick example text.</p>
-                <a href="#" class="card-link">Card link</a>
+                <p className="card-text">Some quick example text.</p>
+                <a href="#" className="card-link">Card link</a>
             </div>
         </div>
     </div>
